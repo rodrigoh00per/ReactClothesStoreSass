2fbe41e69d9f6b3708bb3e15250a07773ca810fa
Rodrigo Guerrero <rodrigoh00per1@gmail.com>
2019-12-19 23:26:56 -0600

    implementation of checkout

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { connect } from "react-redux";
+import { clearItemFromCart } from "../../redux/cart/cart.actions";
 
 import "./checkout-item.styles.scss";
 
-const CheckOutItem = ({ cartItem: { name, imageUrl, quantity, price } }) => {
+const CheckOutItem = ({ cartItem, clearItem }) => {
+  const { name, imageUrl, quantity, price } = cartItem;
   return (
     <div className="checkout-item">
       <div className="image-container">
@@ -11,9 +14,13 @@ const CheckOutItem = ({ cartItem: { name, imageUrl, quantity, price } }) => {
       <span className="name">{name}</span>
       <span className="quantity">{quantity}</span>
       <span className="price">{price}</span>
-      <span className="remove-button">&#9747;</span>
+      <span className="remove-button" onClick={() => clearItem(cartItem)}>
+        &#9747;
+      </span>
     </div>
   );
 };
-
-export default CheckOutItem;
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearItemFromCart(item))
+});
+export default connect(null, mapDispatchToProps)(CheckOutItem);
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -7,3 +7,8 @@ export const toogleCartHidden = () => ({
 export const addItem = item => {
   return { type: CartActionTypes.ADD_ITEM, payload: item };
 };
+
+export const clearItemFromCart = item => {
+  return { type: CartActionTypes.CLEAR_ITEM_FROM_CART, payload: item };
+};
+
