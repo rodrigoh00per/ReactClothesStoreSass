1f6b5e50b257cb727d2899d61d5abc3710f64646
Rodrigo Guerrero Rocha <rodrigoh00per1@gmail.com>
2020-01-07 10:34:13 -0600

    cambio en estilo

--- src/pages/homepage/HomePage.component.jsx
+++ src/pages/homepage/HomePage.component.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./homepage.styles.scss";
+import HomePageContainer from "./HomePage.styles";
 import Directory from "../../components/directory/directory.component";
 
-const HomePage = () => {
-  return (
-    <div className="homepage">
-      <Directory></Directory>
-    </div>
-  );
-};
+const HomePage = () => (
+  <HomePageContainer>
+    <Directory></Directory>
+  </HomePageContainer>
+);
 
 export default HomePage;
