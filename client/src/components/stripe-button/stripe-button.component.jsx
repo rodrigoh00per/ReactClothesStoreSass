6f4d379286884f526ab5933c1e90cfdf19bfc612
Rodrigo Guerrero <rodrigoh00per1@gmail.com>
2020-02-03 14:08:56 -0600

    implementation of client folder

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
-  const onToken = token => {
-    console.log(token);
-    alert("payment succesfull");
-  };
-
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_FGLKHveD4TvowqThXdNn2OSI";
+
+  const onToken = token => {
+    console.log(token);
+    axios({
+      url:"payment",
+      baseURL:"http://localhost:5000",
+      method: "post",
+      data: { amount: priceForStripe, token }
+    })
+      .then(res => alert("your payment was succesfull"))
+      .catch(error => {
+        console.log("Payment Error: ", error);
+        alert(
+          "There was an issue with your payment! Please make sure you use the provided credit card."
+        );
+      });
+  };
 
   return (
     <StripeCheckout
